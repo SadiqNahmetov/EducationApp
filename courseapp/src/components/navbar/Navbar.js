de8b8a130ccb70ef340d3bfe23e621b3eebfc450
SadiqNahmetov <sadigfn@code.edu.az>
2023-04-04 12:23:04 +0400

    Create react navbar

---
+++ courseapp/src/components/navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Navbar.scss';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import { FiPhoneCall } from "react-icons/fi";
+
+function MyNavbar() {
+
+  return (
+
+    <Navbar className='navArea'>
+      <div className='myNavbar'>
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <div className='logo'>
+              <img src="./images/logo.png.webp" alt="" />
+              <span>Course</span>
+            </div>
+            <Nav className="me-auto my-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">About Us</Nav.Link>
+              <Nav.Link href="#link">Courses</Nav.Link>
+              <Nav.Link href="#link">Elements</Nav.Link>
+              <Nav.Link href="#link">News</Nav.Link>
+              <Nav.Link href="#link">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </div>
+      <div className='contact'>
+        <FiPhoneCall  size="30px"/>
+        <span>+994 50 500 50 50</span>
+      </div>
+    </Navbar>
+
+  )
+}
+
+export default MyNavbar;
