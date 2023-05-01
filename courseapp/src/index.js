c7564349a699a9a712561eb89b9c46187429728c
SadiqNahmetov <sadigfn@code.edu.az>
2023-05-01 12:27:43 +0400

    FIxed: ScrollTop

--- courseapp/src/index.js
+++ courseapp/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ScrollTop from './components/scroll/ScrollTop';
+import ScrollTop from './assets/helpers/ScrollTop';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
